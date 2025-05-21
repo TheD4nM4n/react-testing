308284172b78412f790f7c86d13612d8692b86a4
Daniel Poe <thed4nm4n@gmail.com>
2025-05-21 15:38:27 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import ListGroup from "./components/ListGroup";
+import Message from "./Message";
+
+function App() {
+
+  return <div><ListGroup /></div>;
+}
+
+export default App;
---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+function ListGroup() {
+    let items = ['NYC', 'SFC', 'Paris', 'Rome'];
+    const [selectedIndex, setSelectedIndex] = useState(-1);
+
+    return (
+    <>
+        <h1>List</h1>
+        { items.length === 0 && <p>No item found.</p>} 
+        <ul className="list-group">
+            {items.map((item, index) => (
+                <li 
+                    className={
+                        selectedIndex === index
+                        ? "list-group-item active"
+                        : "list-group-item"}
+                    key={item}
+                    onClick={(e) => setSelectedIndex(index)}
+                >
+                    {item}
+                </li>)
+            )
+            }
+        </ul>
+    </>
+  );
+}
+
+export default ListGroup;
