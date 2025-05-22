7c79460b895bbec1ac8973e2eff90a5e4a028921
Justin Joseph <jayjtrainer@gmail.com>
2025-05-22 19:52:35 -0400

    Added a card to JustinComponent

--- src/components/JustinComponent.tsx
+++ src/components/JustinComponent.tsx
@@ -2,7 +2,18 @@ function JustinComponent() {
   return (
     <div>
       <h1>Justin Component</h1>
-      <p>This is a component created by Justin.</p>
+      <p>This is a component.</p>
+      <div class="card">
+        <div class="card-header">
+          I'm very hungry
+        </div>
+        <div class="card-body">
+          pls donate monies :)
+        </div>
+                  <button type="button" class="btn btn-primary">
+            <a>Donate</a>
+          </button>
+      </div>
     </div>
   );
 }
