0aacc57898ee0fbebef61a7bd163e55272ccb6f7
Justin Joseph <jayjtrainer@gmail.com>
2025-06-05 19:09:19 -0400

    Finished ImageSlider

--- src/components/ImageSlider/ImageSlider.tsx
+++ src/components/ImageSlider/ImageSlider.tsx
@@ -1,4 +1,6 @@
+import { ArrowLeft, ArrowRight } from "@mui/icons-material";
 import { useEffect, useState } from "react";
+import "./styles.css";
 
 function ImageSlider({ url, limit = 5, page = 1 }) {
   const [images, setImages] = useState([]);
@@ -8,9 +10,8 @@ function ImageSlider({ url, limit = 5, page = 1 }) {
 
   async function fetchImages(getUrl) {
     try {
-
-        setLoading(true)
-      console.log(`${getUrl}?page=${page}&limit=${limit}`)
+      setLoading(true);
+      console.log(`${getUrl}?page=${page}&limit=${limit}`);
       const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
       const data = await response.json();
 
@@ -20,11 +21,19 @@ function ImageSlider({ url, limit = 5, page = 1 }) {
       }
     } catch (e) {
       setErrorMsg(e.Message);
+      setLoading(false);
     }
   }
-  useEffect(() => 
-    {if (url !== "") fetchImages(url);   
-    }, [url]);
+
+  function handlePrevious() {
+    setCurrentSlide(currentSlide === 0 ? images.length - 1 : currentSlide - 1);
+  }
+  function handleNext() {
+    setCurrentSlide(currentSlide === images.length - 1 ? 0 : currentSlide + 1);
+  }
+  useEffect(() => {
+    if (url !== "") fetchImages(url);
+  }, [url]);
 
   console.log(images);
 
@@ -35,18 +44,41 @@ function ImageSlider({ url, limit = 5, page = 1 }) {
     return <div>Error occured! {errorMsg}</div>;
   }
 
-  return <div>
-    {
-        images && images.length ? images.map(imageItem => 
-            (<img
-            key={imageItem.id}
-            alt={imageItem.download_url}
-            src={imageItem.download_url}
-            className="current-image"
-            />)
-        )
-    : null}
-  </div>;
+  return (
+    <div className="container">
+      <ArrowLeft className="arrow arrow-left" onClick={handlePrevious} />
+      {images && images.length
+        ? images.map((imageItem, index) => (
+            <img
+              key={imageItem.id}
+              alt={imageItem.download_url}
+              src={imageItem.download_url}
+              className={
+                currentSlide === index
+                  ? "current-image"
+                  : "current-image hide-current-image"
+              }
+            />
+          ))
+        : null}
+      <ArrowRight className="arrow arrow-right" onClick={handleNext} />
+      <span className="circle-indicators">
+        {images && images.length
+          ? images.map((_, index) => (
+              <button
+                key={index}
+                className={
+                  currentSlide === index
+                    ? "current-indicator"
+                    : "current-indicator inactive-indicator"
+                }
+                onClick={() => setCurrentSlide(index)}
+              />
+            ))
+          : null}
+      </span>
+    </div>
+  );
 }
 
 export default ImageSlider;
