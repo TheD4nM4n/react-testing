87b52e1bb0eb44ad7dcb34f5feee00c4a2b8de41
Justin Joseph <jayjtrainer@gmail.com>
2025-06-13 10:16:34 -0400

    Made the grid layout in JustinPage a little better

--- src/pages/JustinPage/JustinPage.tsx
+++ src/pages/JustinPage/JustinPage.tsx
@@ -3,6 +3,8 @@ import Accordian from "../../components/Accordian/Accordian";
 import RCG from "../../components/RCG/RCG";
 import StarRating from "../../components/StarRating/StarRating"
 import ImageSlider from "../../components/ImageSlider/ImageSlider"
+import { Grid } from "@mui/material";
+import styles from "./JustinPage.module.css"
 
 function JustinPage() {
   return (
@@ -15,12 +17,12 @@ function JustinPage() {
           Be careful in here, it's scary
         </i>
       </h2>
-      <div className="card-container">
-        <JustinComponent />
-        <Accordian />
-        <RCG />
-        <StarRating numberOfStars={10}/>
-        <ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'}/>
+      <div className={styles.cardcontainer}>
+        <div className="item"><JustinComponent /></div>
+        <div className="item"><Accordian /></div>
+        <div className="item"><RCG /></div>
+        <div className="item"><StarRating numberOfStars={10}/></div>
+        <div className="item"><ImageSlider url={'https://picsum.photos/v2/list'} limit={'10'} page={'1'}/></div>
       </div>
   </div>
   );
